81e48198258cd0767a8c43d9c227768801a90518
Tales Costa <talonacontato@gmail.com>
2023-11-09 11:28:31 -0300

    Add files via upload

--- src/app/components/Skills/Skills.jsx
+++ src/app/components/Skills/Skills.jsx
@@ -8,72 +8,80 @@ const Skills = () => {
           </h2>
 
           <p class="mx-auto max-w-screen-md text-center text-white md:text-lg">
-            This is a section of some simple filler text, also known as
-            placeholder text. It shares some characteristics of a real written
-            text but is random or otherwise generated.
+            Com habilidades afiadas e uma paixão por aprendizado contínuo, estou
+            sempre pronto para enfrentar desafios e contribuir de maneira
+            impactante, seja nas áreas de tecnologia, criatividade ou resolução
+            de problemas.
           </p>
         </div>
 
         <div class="grid gap-8 sm:grid-cols-2 sm:gap-y-10 xl:grid-cols-3">
           <div class="relative rounded-lg bg-zinc-900 p-5 pt-8">
             <h3 class="mb-3 text-lg font-semibold text-purple-500 md:text-xl">
-              How does the product work?
+              Design de interfaces
             </h3>
             <p class="text-white">
-              This is a section of some simple filler text, also known as
-              placeholder text. It shares some characteristics of a real written
-              text.
+              Desenvolvo designs de interfaces que unem estética e
+              funcionalidade, priorizando a experiência do usuário. Crio
+              soluções visuais que comunicam efetivamente e elevam a interação
+              digital.
             </p>
           </div>
 
           <div class="relative rounded-lg bg-zinc-900 p-5 pt-8">
             <h3 class="mb-3 text-lg font-semibold text-purple-500 md:text-xl">
-              How does the product work?
+              Usabilidade
             </h3>
             <p class="text-white">
-              This is a section of some simple filler text, also known as
-              placeholder text. It shares some characteristics of a real written
-              text.
+              Meu foco na usabilidade se traduz em criar experiências
+              intuitivas, onde cada interação é pensada para proporcionar
+              eficiência e satisfação aos usuários, garantindo acessibilidade e
+              facilidade de uso.
             </p>
           </div>
           <div class="relative rounded-lg bg-zinc-900 p-5 pt-8">
             <h3 class="mb-3 text-lg font-semibold text-purple-500 md:text-xl">
-              How does the product work?
+              UX/UI
             </h3>
             <p class="text-white">
-              This is a section of some simple filler text, also known as
-              placeholder text. It shares some characteristics of a real written
-              text.
+              Na interseção entre UX e UI, busco a fusão perfeita entre
+              funcionalidade e beleza, criando designs que não apenas agradam
+              visualmente, mas também proporcionam uma experiência fluida e
+              envolvente para os usuários.
             </p>
           </div>
           <div class="relative rounded-lg bg-zinc-900 p-5 pt-8">
             <h3 class="mb-3 text-lg font-semibold text-purple-500 md:text-xl">
-              How does the product work?
+              HTML 5, CSS 3 e JavaScript
             </h3>
             <p class="text-white">
-              This is a section of some simple filler text, also known as
-              placeholder text. It shares some characteristics of a real written
-              text.
+              Domino HTML5, CSS3 e JavaScript, traduzindo ideias em páginas web
+              interativas e estilizadas. Minha habilidade técnica se combina com
+              uma abordagem criativa para oferecer experiências digitais
+              impactantes e responsivas."
             </p>
           </div>
           <div class="relative rounded-lg bg-zinc-900 p-5 pt-8">
             <h3 class="mb-3 text-lg font-semibold text-purple-500 md:text-xl">
-              How does the product work?
+              Prototipagem
             </h3>
             <p class="text-white">
-              This is a section of some simple filler text, also known as
-              placeholder text. It shares some characteristics of a real written
-              text.
+              Através da prototipagem, transformo conceitos em realidade
+              tangível, permitindo a validação e aprimoramento iterativo de
+              ideias antes da implementação final. Minha abordagem ágil na
+              prototipagem contribui para a criação de soluções mais eficientes
+              e alinhadas às necessidades do usuário.
             </p>
           </div>
           <div class="relative rounded-lg bg-zinc-900 p-5 pt-8">
             <h3 class="mb-3 text-lg font-semibold text-purple-500 md:text-xl">
-              How does the product work?
+              React e TailWind
             </h3>
             <p class="text-white">
-              This is a section of some simple filler text, also known as
-              placeholder text. It shares some characteristics of a real written
-              text.
+              Com React e Tailwind CSS, desenvolvo interfaces dinâmicas e
+              estilizadas. A combinação da eficiência do React com a
+              flexibilidade do Tailwind permite criar aplicações web modernas,
+              mantendo um código limpo e componentes reutilizáveis.
             </p>
           </div>
         </div>
