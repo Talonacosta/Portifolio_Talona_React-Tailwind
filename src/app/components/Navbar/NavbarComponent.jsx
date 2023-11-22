65d29232d9ff8747fca7960f5ea824023edf98a6
Tales Costa <talonacontato@gmail.com>
2023-11-21 21:42:44 -0300

    Add files via upload

--- src/app/components/Navbar/NavbarComponent.jsx
+++ src/app/components/Navbar/NavbarComponent.jsx
@@ -1,47 +1,49 @@
-import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 
 const Navbar = () => {
   return (
     <>
-      <header class="text-white">
-        <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-          <a
-            href="/"
-            class="flex title-font font-medium items-center text-white mb-4 md:mb-0"
-          >
-            <Image src="/svgs/Union.svg" alt="rosa" width={35} height={35} />
-            <span class="ml-3 text-2xl">Talona</span>
-          </a>
-          <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center gap-x-8">
+      <header>
+        <div className="container mx-auto p-5 flex-col md:flex-row items-center">
+          <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center gap-x-8">
             <Link href="/Sobre">
-              <span className="text-purple-500 ">01.</span> Sobre Mim
+              <span className="text-[#6800B4] text-center font-roboto text-xl font-normal leading-normal tracking-wider">
+                01.
+              </span>{" "}
+              <span className="text-black font-roboto text-xl font-normal leading-normal tracking-wide">
+                PROTIFOLIO
+              </span>
             </Link>
             <Link href="/Flores">
-              <span className="text-purple-500">02.</span>Flores
+              <span className="text-[#6800B4] text-center font-roboto text-xl font-normal leading-normal tracking-wider">
+                02.
+              </span>{" "}
+              <span className="text-black font-roboto text-xl font-normal leading-normal tracking-wide">
+                RESUME
+              </span>
             </Link>
+            <a
+              href="/"
+              className="flex title-font font-medium items-center text-black mb-4 md:mb-0"
+            >
+              <Image src="/img/Union.svg" alt="rosa" width={100} height={92} />
+            </a>
             <Link href="/Projetos">
-              <span className="text-purple-400 ">03.</span>Projetos
+              <span className="text-[#6800B4] text-center font-roboto text-xl font-normal leading-normal tracking-wider">
+                03.
+              </span>{" "}
+              <span className="text-black font-roboto text-xl font-normal leading-normal tracking-wide">
+                ABOUT
+              </span>
             </Link>
             <Link href="/Links">
-              <span className="text-purple-500">04.</span>Meus Links
-            </Link>
-            <Link href="/">
-              <button class="inline-flex items-center bg-purple-700 border-0 py-1 px-3 focus:outline-none hover:bg-purple-600 rounded text-base mt-4 md:mt-0 ">
-                Home
-                <svg
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  class="w-4 h-4 ml-1"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M5 12h14M12 5l7 7-7 7"></path>
-                </svg>
-              </button>
+              <span className="text-[#6800B4] text-center font-roboto text-xl font-normal leading-normal tracking-wider">
+                04.
+              </span>{" "}
+              <span className="text-black font-roboto text-xl font-normal leading-normal tracking-wide">
+                CONTACT
+              </span>
             </Link>
           </nav>
         </div>
--- src/pages/Flores.jsx
+++ src/pages/Flores.jsx
@@ -1,70 +1,16 @@
 import React from "react";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import Navbar from "../app/components/Navbar/NavbarComponent";
 
 const Flores = () => {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    autoplay: true,
-    autoplaySpeed: 3000
-  };
-
- 
-
-  const svgStyles = {
-    flor1: {
-      width: "260px",
-      height: "260px",
-      fill: "red",
-    },
-    flor2: {
-      width: "260px",
-      height: "260px",
-      fill: "blue",
-    },
-    flor3: {
-      width: "260px",
-      height: "260px",
-      fill: "red",
-    },
-    flor4: {
-      width: "260px",
-      height: "260px",
-      fill: "red",
-    },
-  };
-
   return (
-    <>
+    <div>
       <Navbar />
-      <h1 className="mb-4 text-center text-2xl text-purple-500 md:mb-6 lg:text-3xl">
-        Flores
-      </h1>
-      <p className="mx-auto max-w-screen-md text-center text-white md:text-lg">
+      <h1>Flores</h1>
+      <p>
         Explore a beleza das flores e descubra informações interessantes sobre
         elas...
       </p>
-      <Slider {...settings} className="justfy-center m-8">
-        <div>
-          <img src="/svgs/florborda.svg" alt="Flor 1" style={svgStyles.flor1} />
-        </div>
-        <div>
-          <img src="/svgs/florborda.svg" alt="Flor 2" style={svgStyles.flor2} />
-        </div>
-        <div>
-          <img src="/svgs/florborda.svg" alt="Flor 3" style={svgStyles.flor3} />
-        </div>
-        <div>
-          <img src="/svgs/florborda.svg" alt="Flor 3" style={svgStyles.flor4} />
-        </div>
-      </Slider>
-    </>
+    </div>
   );
 };
 
