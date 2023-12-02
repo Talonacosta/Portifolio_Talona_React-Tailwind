93dc52d4c557f4f5db346a434080a7a6170c60c2
Tales Costa <talonacontato@gmail.com>
2023-12-02 02:56:40 -0300

    Add files via upload

--- src/pages/Projetos.jsx
+++ src/pages/Projetos.jsx
@@ -5,9 +5,18 @@ const Projetos = () => {
   return (
     <div>
       <Navbar />
-      <h1 className="m-auto text-center font-poppis text-3xl text-purple-r">
-        Aqui estão meu projetos{" "}
+
+      <h1 className="text-center mb-4 text-2xl font-bold text-gray-700">
+        Buscando simplicidade, intenção e humanidade em todas as coisas
       </h1>
+      <h2 className="text-center mb-4 text-2xl font-bold text-gray-700">
+        Bú Eu sou Talona
+      </h2>
+      <p className="text-base  text-gray-600">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi.
+        Sed vel lacus id lacus fringilla dapibus. Fusce ullamcorper posuere
+        lectus, et tincidunt velit malesuada vel.
+      </p>
     </div>
   );
 };
--- src/pages/Sobre.jsx
+++ src/pages/Sobre.jsx
@@ -5,16 +5,20 @@ export default function Sobre() {
   return (
     <div>
       <Navbar />
-      <h1 className="text-">Tales Costa</h1>
-      <h2 className="">Front-end Developer From Brazil</h2>
-      <p className="">
+      <div class="p-4">
+        <h2 class="text-2xl font-bold mb-4">Front-end Developer From Brazil</h2>
+        <p class="mb-4 text-lg">
         Sou um experiente desenvolvedor front-end, com habilidades sólidas em
         JavaScript, HTML/CSS e design responsivo. Comprometido com a qualidade,
         tenho experiência em desenvolvimento orientado a testes. Minha paixão é
         criar experiências excepcionais para os usuários. Busco oportunidades
         para contribuir em projetos desafiadores, alinhados com os mais altos
         padrões de usabilidade e design.
-      </p>
+        </p>
+        <a href="#" class="bg-purple-600 text-white px-4 py-2 rounded">
+          Download PDF
+        </a>
+      </div>
     </div>
   );
 }
